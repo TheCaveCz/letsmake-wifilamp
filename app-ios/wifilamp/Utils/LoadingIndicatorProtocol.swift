f5eb11b7845a762c119a2721f34f08ca98f356bb
Tina Stremenova <martinastremenova@gmail.com>
2017-12-05 10:56:47 +0100

    [App] Adding SVProgressHUD as loading indicator & temporarily changing dev team to STRV.

---
+++ app-ios/wifilamp/Utils/LoadingIndicatorProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  LoadingIndicatorProtocol.swift
+//  wifilamp
+//
+//  Created by Martina Stremenova on 04/12/2017.
+//  Copyright © 2017 The Cave. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+protocol LoadingIndicatorProtocol { }
+extension LoadingIndicatorProtocol where Self:UIViewController {
+    
+    func showLoadingIndicator() {
+        DispatchQueue.main.async {
+            SVProgressHUD.show()
+        }
+    }
+    
+    func hideLoadingIndicator() {
+        DispatchQueue.main.async {
+            SVProgressHUD.dismiss()
+        }
+    }
+}
